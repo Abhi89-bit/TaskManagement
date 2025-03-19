16d085818e88a267d45c13b86c92fcf71850aa25
Abhiraj <www.abhirajsingh18@gmail.com>
2025-03-19 16:59:34 +0530

    wsgi

--- tastmanagement/wsgi.py
+++ tastmanagement/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
+# Add your Django project path
+path = '/home/yourusername/TaskManagement'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'TaskManagement.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tastmanagement.settings')
-
 application = get_wsgi_application()
